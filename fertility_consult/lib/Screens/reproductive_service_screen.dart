0a8aa8239c6910d76a3ee9f39255aad51ed07e64
BroCodeByMinds <brocodebyminds@gmail.com>
2023-12-01 21:12:54 +0530

    modified reproductive services

--- fertility_consult/lib/Screens/reproductive_service_screen.dart
+++ fertility_consult/lib/Screens/reproductive_service_screen.dart
@@ -5,10 +5,40 @@ class ReproductiveServicesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Services'),
+        title: Text('Reproductive Services'),
       ),
-      body: Center(
-        child: Text('Reproductive Services Screen'),
+      body: ListView(
+        children: [
+          _buildSection('Donate Sperm'),
+          _buildSection('Donate Egg'),
+          _buildSection('IVF Treatment'),
+          _buildSection('ICSI Treatment'),
+          _buildSection('IUI Treatment'),
+          _buildSection('Infertility Counselling'),
+          _buildSection('Male Infertility Counselling'),
+          _buildSection('Female Infertility Counselling'),
+          _buildSection('FET Treatment'),
+          _buildSection('Embryo Banking'),
+          _buildSection('Surrogacy'),
+          _buildSection('Check Near Clinic'),
+          _buildSection('Call Appointment'),
+          _buildSection('Clinic Appointment'),
+          _buildSection('Knowledge'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSection(String title) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: ListTile(
+        title: Text(title),
+        onTap: () {
+          // Handle tap on each section
+          // Replace with your logic or navigate to the appropriate view
+        },
+        trailing: Icon(Icons.chevron_right),
       ),
     );
   }
