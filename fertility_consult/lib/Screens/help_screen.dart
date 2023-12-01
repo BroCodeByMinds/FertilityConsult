9896737b18a415813903472366b7a965e2006f0a
BroCodeByMinds <brocodebyminds@gmail.com>
2023-12-01 21:06:51 +0530

    Info in HelpView

--- fertility_consult/lib/Screens/help_screen.dart
+++ fertility_consult/lib/Screens/help_screen.dart
@@ -7,8 +7,37 @@ class HelpView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Help'),
       ),
-      body: Center(
-        child: Text('Help Screen'),
+      body: ListView(
+        children: [
+          _buildSection('New Clinic Booking Help',
+              'If you need assistance with booking a new clinic, please contact our support team.'),
+          _buildSection('Technical Issues',
+              'Encountering technical problems? Our technical support team is here to help.'),
+          _buildSection('Donate Sperm Referral Help',
+              'For assistance with donation referrals, please reach out to our support team.'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSection(String header, String content) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              header,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
       ),
     );
   }
