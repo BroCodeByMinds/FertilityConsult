45019a7dee271eed69287846d5cac9dbb2da4bc6
BroCodeByMinds <brocodebyminds@gmail.com>
2023-12-06 00:51:44 +0530

    separated home screen code

--- fertility_consult/lib/main.dart
+++ fertility_consult/lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:fertility_consult/Screens/home_screen.dart';
-import 'package:fertility_consult/Screens/profile_screen.dart';
-import 'package:fertility_consult/Screens/help_screen.dart';
-import 'package:fertility_consult/Screens/reproductive_service_screen.dart';
-import 'package:fertility_consult/Screens/RegistrationScreen.dart';
-
+import 'package:fertility_consult/Screens/registration_screen.dart'; // Import your registration screen
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,64 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
-  final List<Widget> _tabs = [
-    HomeView(),
-    ProfileView(),
-    ReproductiveServicesView(),
-    HelpView(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    bool isUserRegistered = false; // Replace this with your logic to check if the user is registered
-
-    if (!isUserRegistered) {
-      // If the user is not registered, navigate to the registration page
-      return RegistrationScreen(); // Replace RegistrationScreen with your registration page widget
-    }
-
-    return Scaffold(
-      body: _tabs[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.deepPurple,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.house),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Services',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.help),
-            label: 'Help',
-          ),
-        ],
-      ),
+      home: const RegistrationScreen(), // Use RegistrationScreen as the initial screen
     );
   }
 }
