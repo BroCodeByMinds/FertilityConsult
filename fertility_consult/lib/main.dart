4e154f5d6b308f7eb65a665e4b60787af394c6b0
BroCodeByMinds <brocodebyminds@gmail.com>
2023-12-13 23:50:49 +0530

    validating user logged or not

--- fertility_consult/lib/main.dart
+++ fertility_consult/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:fertility_consult/Screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:fertility_consult/Screens/registration_screen.dart'; // Import your registration screen
+import 'package:fertility_consult/models/registration_helper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +17,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const RegistrationScreen(), // Use RegistrationScreen as the initial screen
+      home: FutureBuilder<bool>(
+        future: RegistrationHelper.checkLoginStatus(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // Show a loading indicator while checking login status
+          } else {
+            if (snapshot.hasError) {
+              return const Text('Error'); // Handle error case
+            } else {
+              return snapshot.data! ? const HomeView() : const RegistrationScreen();
+              // Use HomeView as the initial screen if user is logged in, otherwise use LoginRegistrationScreen
+            }
+          }
+        },
+      ),
     );
   }
 }
+
