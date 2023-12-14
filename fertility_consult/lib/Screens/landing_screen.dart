81d5222822bf19fbf18faef8cf1f8791b8d1e000
BroCodeByMinds <brocodebyminds@gmail.com>
2023-12-14 17:50:47 +0530

    Appointment functionality in Landing page

--- fertility_consult/lib/Screens/landing_screen.dart
+++ fertility_consult/lib/Screens/landing_screen.dart
@@ -1,49 +1,57 @@
 import 'package:fertility_consult/Screens/registration_screen.dart';
+import 'package:fertility_consult/Utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class LandingScreen extends StatelessWidget {
-  const LandingScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Welcome to Fertility Consult'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome!',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Image(
+            image: AssetImage('assets/images/IVF_image3.jpeg'), // Replace with your welcome image asset
+            // Add image properties as needed
+          ),
+          const SizedBox(height: 20), // Add spacing between image and text
+          const Text(
+            'Are you Married and facing Infertility Problems?\nBook your appointment today!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Fertility Consult App',
-              style: TextStyle(
-                fontSize: 20,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the appropriate screen (Login, Registration, Home, etc.)
-                // You can replace `LoginRegistrationScreen()` with the intended screen
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegistrationScreen()),
-                );
-              },
-              child: const Text('Get Started'),
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20), // Add spacing between text and dropdown
+          _buildDropdown(context),
+        ],
       ),
+    );
+  }
+
+  Widget _buildDropdown(BuildContext context) {
+    return DropdownButton<String>(
+      hint: const Text('Select a service'), // Placeholder text
+      items: Constants.reproductiveServices
+          .map((String service) {
+            return DropdownMenuItem<String>(
+              value: service,
+              child: Text(service),
+            );
+          })
+          .toList(),
+      onChanged: (String? selectedService) {
+        // Handle selected service
+        if (selectedService != null) {
+          // Perform actions based on the selected service
+          print('Selected service: $selectedService');
+          // Add logic for handling the selected service
+        }
+      },
     );
   }
 }
