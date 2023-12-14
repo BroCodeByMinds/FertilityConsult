20d3f71075f6ddf79c7cc037fb08491b23c586e9
BroCodeByMinds <brocodebyminds@gmail.com>
2023-12-14 17:52:29 +0530

    cleaned user service

--- fertility_consult/lib/services/user_service.dart
+++ fertility_consult/lib/services/user_service.dart
@@ -7,12 +7,7 @@ Future<bool> checkUserRegistration() async {
     var response = await http.get(Uri.parse(checkRegistrationUrl));
 
     if (response.statusCode == 200) {
-      // Parse the response data and determine if the user is registered
-      // Replace this logic with how you handle the response from your backend
-      // For example:
-      // var responseData = json.decode(response.body);
-      // return (responseData['userRegistered'] == true);
-      return true; // Replace this with your logic
+      return true; 
     } else {
       return false;
     }
